c8c0928ce61982369d298a9e43dd4902395c407b
JoseParedezz <joseparedez599@gmail.com>
2025-05-29 14:42:40 -0300

    zxzx

---
+++ TP8_Grupo_8/DATOS/AccesoDatos.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATOS
+{
+    internal class AccesoDatos
+    {
+        string stringConexion = @"Data Source=localhost\sqlexpress;Initial Catalog=BDSucursales;Integrated Security=True";
+
+        public SqlConnection ObtenerConexion()
+        {
+            SqlConnection sqlConnection = new SqlConnection(stringConexion);
+            try
+            {
+                sqlConnection.Open();
+                return sqlConnection;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+        }
+
+        public SqlDataAdapter ObtenerAdapdator(string consultaSQL)
+        {
+            SqlDataAdapter sqlAdapter;
+            try
+            {
+                sqlAdapter = new SqlDataAdapter(consultaSQL, ObtenerConexion());
+                return sqlAdapter;
+            }
+            catch (Exception exception)
+            {
+                return null;
+            }
+        }
+
+        public DataTable ObtenerTabla(string nombreTabla, string consultaSQL)
+        {
+            DataSet dataSet = new DataSet();
+            AccesoDatos datos = new AccesoDatos();
+            SqlDataAdapter sqlDataAdapter = datos.ObtenerAdapdator(consultaSQL);
+            sqlDataAdapter.Fill(dataSet, nombreTabla);
+            return dataSet.Tables[nombreTabla];
+        }
+    }
+}
