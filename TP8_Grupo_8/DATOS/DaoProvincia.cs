98b3ffa5d0347eb677bd23d14e2ecb4f6ff458cd
LuckyVAL06 <lopezvalentinoscar@gmail.com>
2025-05-29 21:52:06 -0300

    DaoProvincia chetumaaare
    Free Palpatine

---
+++ TP8_Grupo_8/DATOS/DaoProvincia.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.Sql;
+
+namespace DATOS
+{
+    internal class DaoProvincia
+    {
+        AccesoDatos datos = new AccesoDatos();
+
+        public DataTable getTablaProvincia()
+        {
+            DataTable tabla = datos.ObtenerTabla("Provincia", "SELECT * FROM Provincia");
+            return tabla;
+        }
+    }
+}
