83240829578a8c175f4eb2ee83fd630aac196312
QuinnnDev <joacodvecchia@gmail.com>
2025-05-31 01:34:10 -0300

    mejorado

--- TP8_Grupo_8/ENTIDADES/Sucursal.cs
+++ TP8_Grupo_8/ENTIDADES/Sucursal.cs
@@ -8,70 +8,13 @@ namespace ENTIDADES
 {
     public class Sucursal
     {
-        private String NombreSucursal;
-        private String DescripcionSucursal;
-        private int    IdHorarioSucursal;
-        private int    IdProvinciaSucursal;
-        private String DireccionSucursal;
-        private String UrlImagen_Sucursal;
-        
+        public String NombreSucursal        { get; set; }
+        public String DescripcionSucursal   { get; set; }
+        public int    IdHorarioSucursal     { get; set; }
+        public int    IdProvinciaSucursal   { get; set; }
+        public String DireccionSucursal     { get; set; }
+        public String UrlImagen_Sucursal    { get; set; }
 
-        public string getNombreSucursal()
-        {
-            return NombreSucursal;
-        }
-
-        public void setNombreSucursal(string nombre)
-        {
-            NombreSucursal = nombre;
-        }
-
-        public string getDescripcionSucursal()
-        {
-            return DescripcionSucursal;
-        }
-
-        public void setDescripcionSucursal(string descripcion)
-        {
-            DescripcionSucursal = descripcion;
-        }
-
-        public int getIdHorarioSucursal()
-        {
-            return IdHorarioSucursal;
-        }
-        public void setIdHorarioSucursal(int id)
-        {
-            IdHorarioSucursal = id;
-        }
-
-        public int getIdProvinciaSucursal()
-        {
-            return IdProvinciaSucursal;
-        }
-        public void setIdProvinciaSucursal(int id)
-        {
-            IdProvinciaSucursal = id;
-        }
-
-        public string getDireccionSucursal() 
-        {
-            return DireccionSucursal;
-        }
-
-        public void setDireccionSucursal(string direccion)
-        {
-            DireccionSucursal = direccion;
-        }
-
-        public string getImagenUrl()
-        {
-            return UrlImagen_Sucursal;
-        }
-        public void setImagenUrl(string url)
-        {
-            UrlImagen_Sucursal = url;
-        }
 
 
     }
